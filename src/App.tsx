9fe38aa6eb312c43c6718cef135138c95540eb55
Ismail Gül <ismail.guls@gmail.com>
2023-02-07 17:48:39 +0300

    add date filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { ExpandMore } from "@mui/icons-material";
+import {
+  Accordion,
+  AccordionDetails,
+  Box,
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
 import {
   GoogleMap,
   InfoWindow,
   LoadScript,
   Marker,
 } from "@react-google-maps/api";
+import { isBefore, parse, sub } from "date-fns";
 import { useMemo, useState } from "react";
 import "./App.css";
 import { MAPS_API_KEY } from "./config";
@@ -30,15 +42,44 @@ const map = {
   elazığ: "red",
 };
 
+enum Hour {
+  H1 = "1",
+  H2 = "2",
+  H4 = "4",
+  H8 = "8",
+}
+
 function App() {
   const [tooltipRow, setTooltipRow] = useState<{
     Enlem: number;
     Boylam: number;
   }>();
+  const [hour, setHour] = useState<Hour | null>(null);
+
+  const handleHourFilter = (
+    event: React.MouseEvent<HTMLElement>,
+    newHour: Hour | null
+  ) => {
+    setHour(newHour);
+  };
+
+  const filtered = useMemo(() => {
+    if (!hour) return rows;
+
+    const startHour = sub(new Date(), { hours: +hour });
+    console.log(startHour);
+    const filteredRows = rows.filter((row) => {
+      return isBefore(
+        startHour,
+        parse(row.Tarih, "yyyy-MM-dd HH:mm:ss", new Date())
+      );
+    });
+    return filteredRows;
+  }, [hour]);
 
   const markers = useMemo(
     () =>
-      rows.map((row) => (
+      filtered.map((row) => (
         <Marker
           key={row.URL}
           position={{ lat: row.Enlem, lng: row.Boylam }}
@@ -50,36 +91,64 @@ function App() {
           }}
         ></Marker>
       )),
-    []
+    [filtered]
   );
   return (
-    <LoadScript googleMapsApiKey={MAPS_API_KEY}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        zoom={10}
-        center={center}
-        onClick={() => setTooltipRow(undefined)}
-      >
-        {markers}
-        {tooltipRow && (
-          <InfoWindow
-            position={{ lat: tooltipRow.Enlem, lng: tooltipRow.Boylam }}
-            options={{
-              pixelOffset: { height: -20, equals: () => true, width: 0 },
-            }}
+    <>
+      <LoadScript googleMapsApiKey={MAPS_API_KEY}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          zoom={10}
+          center={center}
+          onClick={() => setTooltipRow(undefined)}
+        >
+          {markers}
+          {tooltipRow && (
+            <InfoWindow
+              position={{ lat: tooltipRow.Enlem, lng: tooltipRow.Boylam }}
+              options={{
+                pixelOffset: { height: -20, equals: () => true, width: 0 },
+              }}
+            >
+              <div>
+                {Object.entries(tooltipRow).map(([key, value]) => (
+                  <>
+                    <pre>{key}</pre>
+                    {value}
+                  </>
+                ))}
+              </div>
+            </InfoWindow>
+          )}
+        </GoogleMap>
+      </LoadScript>
+      <Box position="absolute" top={90} left={10}>
+        <Accordion defaultExpanded>
+          <AccordionSummary
+            expandIcon={<ExpandMore />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
           >
-            <div>
-              {Object.entries(tooltipRow).map(([key, value]) => (
-                <>
-                  <pre>{key}</pre>
-                  {value}
-                </>
-              ))}
-            </div>
-          </InfoWindow>
-        )}
-      </GoogleMap>
-    </LoadScript>
+            <Typography>Filtrele</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Stack direction="row" alignItems="center" spacing={1}>
+              <Typography>Son</Typography>
+              <ToggleButtonGroup
+                exclusive
+                value={hour}
+                onChange={handleHourFilter}
+                size="small"
+              >
+                {Object.values(Hour).map((hour) => (
+                  <ToggleButton value={hour}>{hour} saat</ToggleButton>
+                ))}
+              </ToggleButtonGroup>
+            </Stack>
+          </AccordionDetails>
+        </Accordion>
+      </Box>
+    </>
   );
 }
 
