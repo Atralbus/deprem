b3e03266abf35d5cd3dabf46926069a452632b90
Ismail Gül <ismail.guls@gmail.com>
2023-02-07 15:58:15 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { useMemo } from "react";
+import "./App.css";
+import { MAPS_API_KEY } from "./config";
+import rows from "./data.json";
+
+const containerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+
+const center = {
+  lat: 36.908453,
+  lng: 36.640183,
+};
+
+const map = {
+  hatay: "yellow",
+  kahramanmaraş: "blue",
+  adıyaman: "green",
+  malatya: "ltblue",
+  gaziantep: "orange",
+  diyarbakır: "pink",
+  osmaniye: "purple",
+  elazığ: "red",
+};
 
 function App() {
+  const markers = useMemo(
+    () =>
+      rows.map((row) => (
+        <Marker
+          key={row.URL}
+          position={{ lat: row.Enlem, lng: row.Boylam }}
+          icon={{
+            url: `http://maps.google.com/mapfiles/ms/icons/${
+              map[row.Şehir as keyof typeof map]
+            }-dot.png`,
+          }}
+        />
+      )),
+    []
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LoadScript googleMapsApiKey={MAPS_API_KEY}>
+      <GoogleMap mapContainerStyle={containerStyle} zoom={10} center={center}>
+        {markers}
+      </GoogleMap>
+    </LoadScript>
   );
 }
 
---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const MAPS_API_KEY = process.env.REACT_APP_MAPS_API_KEY || "";
